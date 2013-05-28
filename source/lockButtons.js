5da983ac14d57bc32e474f5913f4445a306a0cae
Travers Buda <tkbuda@gmail.com>
2013-05-27 20:04:09 -0700

    Style

--- source/lockButtons.js
+++ source/lockButtons.js
@@ -1,36 +1,36 @@
 /* Lock button with label */
 enyo.kind({
-        name: "lockToggle",
-        kind: enyo.Control,
-        style: "padding: 10px; margin: 10px;",
+   name: "lockToggle",
+   kind: enyo.Control,
+   style: "padding: 10px; margin: 10px;",
 
-        published: {
-                label: ""
-        },
+   published: {
+      label: ""
+   },
 
-        create: function() {
-                this.inherited(arguments);
-                this.$.labelSubcomponent.setContent(this.label);
-        },
+   create: function() {
+      this.inherited(arguments);
+      this.$.labelSubcomponent.setContent(this.label);
+   },
 
-        components:[
-                {
-                        kind: "enyo.Control",
-                        name: "labelSubcomponent",
-                        tag: "div",
-                        style: "width: 50px; height: 50px; float: left; padding-right: 10px",
-                        content: "default"
-                },
-                {kind: "onyx.ToggleButton", name: "lockButton", onContent: "Locked", offContent: "Unlocked", onChange: "onLockButtonToggle"}
-        ],
+   components:[
+      {
+         kind: "enyo.Control",
+         name: "labelSubcomponent",
+         tag: "div",
+         style: "width: 50px; height: 50px; float: left; padding-right: 10px",
+         content: "default"
+      },
+      {kind: "onyx.ToggleButton", name: "lockButton", onContent: "Locked", offContent: "Unlocked", onChange: "onLockButtonToggle"}
+   ],
 
-        onLockButtonToggle: function(iSender, inEvent) {
-                enyo.Signals.send("onLockButtonToggle", this);
-                return true;
-        },
+   onLockButtonToggle: function(iSender, inEvent) {
+      enyo.Signals.send("onLockButtonToggle", this);
+      return true;
+   },
 
-        getLockButton: function() {
-                return this.$.lockButton;
-        }
+   getLockButton: function() {
+      return this.$.lockButton;
+   }
 });
 
